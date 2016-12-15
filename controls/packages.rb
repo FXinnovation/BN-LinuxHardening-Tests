518e000e3a2479a631a3187223fb1b42b5d82f83
Pascal S. Clermont <p@scal.ca>
2016-12-15 13:33:38 -0500

    BNCA-31 NIS Client should not be installed.

--- controls/packages.rb
+++ controls/packages.rb
@@ -40,3 +40,11 @@ control 'package-05' do
     it { should_not be_installed }
   end
 end
+
+control 'package-06' do
+  impact 1.0
+  title 'Do not install NIS client'
+  describe package('ypbind') do
+    it { should_not be_installed }
+  end
+end
