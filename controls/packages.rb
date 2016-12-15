13352ac9cbc2de019c98d36f125e541c994df87c
Pascal S. Clermont <p@scal.ca>
2016-12-15 13:45:23 -0500

    BNCA-34 tftp server should not be installed.

--- controls/packages.rb
+++ controls/packages.rb
@@ -67,6 +67,14 @@ end
 
 control 'package-09' do
   impact 1.0
+  title 'Do not install tftp server'
+  describe package('tftp-server') do
+    it { should_not be_installed }
+  end
+end
+
+control 'package-10' do
+  impact 1.0
   title 'Do not install talk'
   describe package('talk') do
     it { should_not be_installed }
