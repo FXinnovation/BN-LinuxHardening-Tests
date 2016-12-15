e63bd28322aff70ab134453bf552b87c9ee4995f
Pascal S. Clermont <p@scal.ca>
2016-12-15 14:41:12 -0500

    BNCA-40 Disable chargen-stream service.

--- controls/services.rb
+++ controls/services.rb
@@ -9,3 +9,12 @@ control 'service-01' do
     it { should_not be_running }
   end
 end
+
+control 'service-02' do
+  impact 1.0
+  title 'Character Generator Protocol (CHARGEN) STREAM services'
+  describe systemd_service('chargen-stream') do
+    it { should_not be_enabled }
+    it { should_not be_running }
+  end
+end
