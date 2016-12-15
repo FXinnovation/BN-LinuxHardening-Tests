b30a81b51d72332f179f730468a0b5623593542f
Pascal S. Clermont <p@scal.ca>
2016-12-15 13:35:52 -0500

    BNCA-32 make sure NIS Server is not installed.

--- controls/packages.rb
+++ controls/packages.rb
@@ -48,3 +48,11 @@ control 'package-06' do
     it { should_not be_installed }
   end
 end
+
+control 'package-07' do
+  impact 1.0
+  title 'Do not install NIS server'
+  describe package('ypserv') do
+    it { should_not be_installed }
+  end
+end
