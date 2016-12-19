ac39875f2cea73d23b05652ee6b23fee1818c029
Pascal Clermont <pascal.clermont@videri.com>
2016-12-19 09:16:48 -0500

    BNCA-53 adding tests for sysctl IP forwarding [typo]

--- controls/sysctl.rb
+++ controls/sysctl.rb
@@ -6,7 +6,7 @@ control 'sysctl-01' do
   impact 1.0
   title 'IPv4 Forwarding'
   desc "If you're not intending for your system to forward traffic between interfaces, or if you only have a single interface, the forwarding function must be disable."
-  describe kernel_parameter('net.ipv4.ip_forward'{ do
+  describe kernel_parameter('net.ipv4.ip_forward') do
     its(:value) { should eq 0 }
   end
   describe kernel_parameter('net.ipv4.conf.all.forwarding') do
