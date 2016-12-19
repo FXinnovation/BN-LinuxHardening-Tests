f14d0526cc44905aa25ba9fd5c91ac2d439c78db
Pascal Clermont <pascal.clermont@videri.com>
2016-12-19 12:15:49 -0500

    BNCA-64 make sure that rsyslog.conf is compliant to reqs.

--- controls/rsyslog.rb
+++ controls/rsyslog.rb
@@ -24,3 +24,22 @@ control 'rsyslog-03' do
     it { should_not be_running }
   end
 end
+
+control 'rsyslog-04' do
+  impact 1.0
+  title 'rsyslog Configuration'
+  describe file('/etc/rsyslog.conf') do
+    its('content') { should match(%r{\$FileOwner\ssyslog}) }
+    its('content') { should match(%r{\$FileGroup\ssyslog}) }
+    its('content') { should match(%r{\$FileCreateMode\s0640}) }
+    its('content') { should match(%r{\$DirCreateMode\s0750}) }
+    its('content') { should match(%r{\$Umask\s0027}) }
+    its('content') { should match(%r{\$PrivDropToUser\ssyslog}) }
+    its('content') { should match(%r{\$PrivDropToGroup\ssyslog}) }
+    its('content') { should match(%r{auth\,authpriv\,user\.\*\s\/var\/log\/secure}) }
+    its('content') { should match(%r{kern\.\*\s\/var\/log\/kern.log}) }
+    its('content') { should match(%r{daemon\.\*\s\/var\/log\/daemon.log}) }
+    its('content') { should match(%r{syslog\.\*\s\/var\/log\/syslog}) }
+    its('content') { should match(%r{lpr\,news\,uucp\,local0\,local1\,local2\,local3\,local4\,local5\,local6\.\*\s\/var\/log\/unused.log}) }
+  end
+end
