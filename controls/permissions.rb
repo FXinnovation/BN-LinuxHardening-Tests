14585728250ba3129936aae993eaa6c5a1a230c6
Pascal Clermont <pascal.clermont@videri.com>
2016-12-22 09:17:09 -0500

    BNCA-98 Make sure that all system cron configuration files are owned and only readable by root.

--- controls/permissions.rb
+++ controls/permissions.rb
@@ -19,3 +19,78 @@ control 'permissions-01' do
   end
 end
 
+control 'permissions-02' do
+  title 'Cron configuration files'
+  desc "All system cron files should be owned by root and have proper permissions on them."
+  describe file('/etc/anacrontab') do
+    it { should be_owned_by 'root' }
+    its('group') { should eq 'root' }
+    it { should_not be_writable.by('group') }
+    it { should_not be_readable.by('group') }
+    it { should_not be_executable.by('group') }
+    it { should_not be_writable.by('other') }
+    it { should_not be_executable.by('other') }
+    it { should_not be_readable.by('other') }
+  end
+  describe file('/etc/crontab') do
+    it { should be_owned_by 'root' }
+    its('group') { should eq 'root' }
+    it { should_not be_writable.by('group') }
+    it { should_not be_readable.by('group') }
+    it { should_not be_executable.by('group') }
+    it { should_not be_writable.by('other') }
+    it { should_not be_executable.by('other') }
+    it { should_not be_readable.by('other') }
+  end
+  describe file('/etc/cron.hourly') do
+    it { should be_owned_by 'root' }
+    its('group') { should eq 'root' }
+    it { should_not be_writable.by('group') }
+    it { should_not be_readable.by('group') }
+    it { should_not be_executable.by('group') }
+    it { should_not be_writable.by('other') }
+    it { should_not be_executable.by('other') }
+    it { should_not be_readable.by('other') }
+  end
+  describe file('/etc/cron.daily') do
+    it { should be_owned_by 'root' }
+    its('group') { should eq 'root' }
+    it { should_not be_writable.by('group') }
+    it { should_not be_readable.by('group') }
+    it { should_not be_executable.by('group') }
+    it { should_not be_writable.by('other') }
+    it { should_not be_executable.by('other') }
+    it { should_not be_readable.by('other') }
+  end
+  describe file('/etc/cron.weekly') do
+    it { should be_owned_by 'root' }
+    its('group') { should eq 'root' }
+    it { should_not be_writable.by('group') }
+    it { should_not be_readable.by('group') }
+    it { should_not be_executable.by('group') }
+    it { should_not be_writable.by('other') }
+    it { should_not be_executable.by('other') }
+    it { should_not be_readable.by('other') }
+  end
+  describe file('/etc/cron.monthly') do
+    it { should be_owned_by 'root' }
+    its('group') { should eq 'root' }
+    it { should_not be_writable.by('group') }
+    it { should_not be_readable.by('group') }
+    it { should_not be_executable.by('group') }
+    it { should_not be_writable.by('other') }
+    it { should_not be_executable.by('other') }
+    it { should_not be_readable.by('other') }
+  end
+  describe file('/etc/cron.d') do
+    it { should be_owned_by 'root' }
+    its('group') { should eq 'root' }
+    it { should_not be_writable.by('group') }
+    it { should_not be_readable.by('group') }
+    it { should_not be_executable.by('group') }
+    it { should_not be_writable.by('other') }
+    it { should_not be_executable.by('other') }
+    it { should_not be_readable.by('other') }
+  end
+end
+
