6b2aa7598384e1e59659193578bd1c700da0b45b
Pascal Clermont <pascal.clermont@videri.com>
2016-12-27 15:33:30 -0500

    BNCA-88 verify that sshd configuration is following best practices.

---
+++ controls/ssh_spec.rb
@@ -0,0 +1,23 @@
+# encoding: utf-8
+# author: Pascal S. Clermont
+
+control 'ssh-01' do
+  title 'SSHd configuration'
+  desc "sshd configuration should comply with best practices"
+  describe sshd_config do
+    its('Protocol') { should cmp 2 }
+    its('IgnoreRhosts') { should eq 'yes' }
+    its('HostbasedAuthentication') { should eq 'no' }
+    its('PermitRootLogin') { should eq 'no' }
+    its('PermitEmptyPasswords') { should eq 'no' }
+    its('PubkeyAuthentication') { should eq 'yes' }
+    its('PasswordAuthentication') { should eq 'no' }
+    its('X11Forwarding') { should eq 'no' }
+    its('MaxAuthTries') { should eq 5 }
+    its('Ciphers') { should cmp('aes128-ctr,aes192-ctr,aes256-ctr') }
+    its('ClientAliveInterval') { should eq 900 }
+    its('ClientAliveCountMax') { should eq 0 }
+    its('UsePAM') { should eq 'yes' }
+    its('LogLevel') { should eq 'INFO' }
+  end
+end
