f109543252acb6f8086743dee4422fcdf8b4279f
Pascal Clermont <pascal.clermont@videri.com>
2016-12-27 13:30:01 -0500

    BNCA-131 Verify default user umask.

--- controls/os_spec.rb
+++ controls/os_spec.rb
@@ -9,3 +9,14 @@ control 'os-01' do
   end
 end
 
+control 'os-02' do
+  title 'user UMASK'
+  desc "verify default umask for users to 077"
+  describe file('/etc/profile.d/umask.sh') do
+    its('content') { should match %r{umask\s+077} }
+  end
+  describe file('/etc/bashrc') do
+    its('content') { should match %r{.*\n\s*umask\s+077[^0-9].*} }
+  end
+end
+
