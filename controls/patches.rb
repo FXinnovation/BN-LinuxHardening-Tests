4df84037c3f81290212dc5e536cb8b0bb84f4868
Pascal Clermont <pascal.clermont@videri.com>
2017-01-02 10:38:49 -0500

    BNCA-2 Verify that all the patches have been installed.

---
+++ controls/patches.rb
@@ -0,0 +1,17 @@
+control 'verify-patches' do
+  impact 0.3
+  title "All patches should be installed"
+  describe linux_update do
+    it { should be_uptodate}
+  end
+end
+
+control 'patches' do
+  impact 0.3
+  title "All updates are installed"
+  linux_update.updates.each { |update|
+    describe package(update['name']) do
+      its('version') { should eq update['version']}
+    end
+  }
+end
