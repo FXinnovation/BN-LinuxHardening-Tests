11ecb30838596e9489baeb4eed7aa32998c50424
Pascal Clermont <pascal.clermont@videri.com>
2017-01-04 14:18:18 -0500

    BNCA-97 verify that system accounts cannot get a shell.

---
+++ controls/accounts.rb
@@ -0,0 +1,14 @@
+# encoding: utf-8
+# author: Pascal S. Clermont
+
+control 'accounts-01' do
+  title 'System Accounts'
+  desc "system accounts should not be able to login"
+  describe passwd.shells('/bin/bash') do
+    its('uids') { should_not include(/^[1-9][0-9]?[0-9]?$/) }
+  end
+  describe passwd.shells('/bin/sh') do
+    its('uids') { should_not include(/^[1-9][0-9]?[0-9]?$/) }
+  end
+end
+
