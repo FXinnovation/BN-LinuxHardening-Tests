2f63721e7fc70b1bf01d07e6a38b3ec075ffa9a6
Pascal Clermont <pascal.clermont@videri.com>
2017-01-07 13:45:08 -0500

    BNCA-121 make sure that home directories are owned by proper user.

--- controls/permissions.rb
+++ controls/permissions.rb
@@ -225,3 +225,10 @@ control 'permissions-12' do
   end
 end
 
+control 'permissions-13' do
+  title 'Home Directories'
+  desc 'Home directories should be owned by their users.'
+  describe command("cat /etc/passwd | awk -F: '{ print $1 \" \" $3 \" \" $6 }' | while read user uid dir; do if [ $uid -ge 1000 -a -d \"$dir\" -a $user != \"nfsnobody\" ]; then owner=$(stat -L -c \"%U\" \"$dir\") ; if [ \"$owner\" != \"$user\" ]; then echo \"The home directory ($dir) of user $user is owned by $owner.\" ; fi ; fi ; done") do
+    its('stdout') { should eq '' }
+  end
+end
