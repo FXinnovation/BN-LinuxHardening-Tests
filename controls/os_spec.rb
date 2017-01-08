a31615f1eb39f3c73b64ba2ab3ec701eabb2bad0
Pascal Clermont <pascal.clermont@videri.com>
2017-01-08 11:20:19 -0500

    more code cleanup

--- controls/os_spec.rb
+++ controls/os_spec.rb
@@ -5,7 +5,7 @@ control 'os-01' do
   title 'Daemon UMASK'
   desc 'daemon umask should be set to 027'
   describe file('/etc/sysconfig/init') do
-    its('content') { should match %r{.*\numask\s+027[^0-9].*} }
+    its('content') { should match(%r{.*\numask\s+027[^0-9].*}) }
   end
 end
 
@@ -13,10 +13,10 @@ control 'os-02' do
   title 'user UMASK'
   desc 'verify default umask for users to 077'
   describe file('/etc/profile.d/umask.sh') do
-    its('content') { should match %r{umask\s+077} }
+    its('content') { should match(%r{umask\s+077}) }
   end
   describe file('/etc/bashrc') do
-    its('content') { should match %r{.*\n\s*umask\s+077[^0-9].*} }
+    its('content') { should match(%r{.*\n\s*umask\s+077[^0-9].*}) }
   end
 end
 
@@ -24,13 +24,13 @@ control 'os-03' do
   title 'Login Banner'
   desc 'verify login banner does not contain sensitive information.'
   describe file('/etc/motd') do
-    its('content') { should_not match %r{.*\\[mrsv].*} }
+    its('content') { should_not match(%r{.*\\[mrsv].*}) }
   end
   describe file('/etc/issue') do
-    its('content') { should_not match %r{.*\\[mrsv].*} }
+    its('content') { should_not match(%r{.*\\[mrsv].*}) }
   end
   describe file('/etc/issue.net') do
-    its('content') { should_not match %r{.*\\[mrsv].*} }
+    its('content') { should_not match(%r{.*\\[mrsv].*}) }
   end
 end
 
--- controls/sysctl.rb
+++ controls/sysctl.rb
@@ -14,7 +14,7 @@ end
 control 'sysctl-02' do
   impact 1.0
   title 'IPv4 Forwarding'
-  desc "If you're not intending for your system to forward traffic between interfaces, or if you only have a single interface, the forwarding function must be disable."
+  desc 'If you are not intending for your system to forward traffic between interfaces, or if you only have a single interface, the forwarding function must be disable.'
   describe kernel_parameter('net.ipv4.ip_forward') do
     its(:value) { should eq 0 }
   end
@@ -26,7 +26,7 @@ end
 control 'sysctl-03' do
   impact 1.0
   title 'Packet Redirects'
-  desc "If you're not intending for your system to redirecting packets, the redirect function must be disable." 
+  desc 'If you are not intending for your system to redirecting packets, the redirect function must be disable.'
   describe kernel_parameter('net.ipv4.conf.all.send_redirects') do
     its(:value) { should eq 0 }
   end
