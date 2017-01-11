5a6393f28f14fd2a25df4e79fd18a1db39607315
Pascal Clermont <pascal.clermont@videri.com>
2017-01-11 12:48:08 -0500

    BNCA-74 Record Events That Modify System's Network Environment

--- controls/audit.rb
+++ controls/audit.rb
@@ -21,3 +21,16 @@ control 'audit-02' do
     its('lines') { should contain_match(%r{-w /etc/localtime -p wa -k time-change}) }
   end
 end
+
+control 'audit-03' do
+  title 'Audit network'
+  desc 'the Audit Configuration should Record Events That Modify System\'s Network Environment'
+  describe auditd_rules do
+    its('lines') { should contain_match(%r{-a always,exit -F arch=b64 -S sethostname -S setdomainname -k system-locale}) }
+    its('lines') { should contain_match(%r{-a always,exit -F arch=b32 -S sethostname -S setdomainname -k system-locale}) }
+    its('lines') { should contain_match(%r{-w /etc/issue -p wa -k system-locale}) }
+    its('lines') { should contain_match(%r{-w /etc/issue.net -p wa -k system-locale}) }
+    its('lines') { should contain_match(%r{-w /etc/hosts -p wa -k system-locale}) }
+    its('lines') { should contain_match(%r{-w /etc/sysconfig/network -p wa -k system-locale}) }
+  end
+end
