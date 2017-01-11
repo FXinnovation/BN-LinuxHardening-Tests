a1bb997e6886da57bd814b7f60889a3b43836d46
Pascal Clermont <pascal.clermont@videri.com>
2017-01-11 12:27:41 -0500

    BNCA-72 Record Events That Modify Date and Time Information

--- controls/audit.rb
+++ controls/audit.rb
@@ -9,3 +9,15 @@ control 'audit-01' do
     its('lines') { should contain_match(%r{-e 2}) }
   end
 end
+
+control 'audit-02' do
+  title 'Audit Date / time'
+  desc 'the Audit Configuration should Record Events That Modify Date and Time Information'
+  describe auditd_rules do
+    its('lines') { should contain_match(%r{-a always,exit -F arch=b64 -S adjtimex -S settimeofday -k time-change}) }
+    its('lines') { should contain_match(%r{-a always,exit -F arch=b32 -S adjtimex -S settimeofday -S stime -k time-change}) }
+    its('lines') { should contain_match(%r{-a always,exit -F arch=b64 -S clock_settime -k time-change}) }
+    its('lines') { should contain_match(%r{-a always,exit -F arch=b32 -S clock_settime -k time-change}) }
+    its('lines') { should contain_match(%r{-w /etc/localtime -p wa -k time-change}) }
+  end
+end
